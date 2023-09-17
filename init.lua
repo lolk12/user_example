ce52e7dfe5e76c5e1e5eac82371bbaf85ccb8855
Alexey Kolpakov <i@alexey-kolpakov.ru>
2023-09-18 01:16:33 +0300

    add my config nvim

--- init.lua
+++ init.lua
@@ -18,7 +18,9 @@ return {
   },
 
   -- Set colorscheme to use
-  colorscheme = "astrodark",
+  colorscheme = "catppuccin",
+  plugins = {
+  },
 
   -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
   diagnostics = {
--- plugins/user.lua
+++ plugins/user.lua
@@ -1,6 +1,29 @@
 return {
   -- You can also add new plugins here as well:
   -- Add plugins, the lazy syntax
+  "gelguy/wilder.nvim",
+
+  "jose-elias-alvarez/null-ls.nvim",
+  "neovim/nvim-lspconfig",
+  "MunifTanjim/eslint.nvim",
+
+  -- Theme START
+  "projekt0n/github-nvim-theme",
+  "catppuccin/nvim",
+  {
+    "rose-pine/neovim",
+    name = "rose-pine",
+    config = function()
+      require('rose-pine').setup({
+        --- @usage 'auto'|'main'|'moon'|'dawn'
+        variant = 'moon',
+        --- @usage 'main'|'moon'|'dawn'
+        dark_variant = 'moon',
+      })
+    end,
+  },
+  -- Theme END
+
   -- "andweeb/presence.nvim",
   -- {
   --   "ray-x/lsp_signature.nvim",
